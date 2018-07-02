88d681aee2be58a7c1de7d541ad5365463ff5bc2
Mohammed Niyas <Mohammedniyas@MSSPLLAP066.meritgroup.com>
2018-07-02 19:41:18 +0100

    added post endpoint for user

--- app.js
+++ app.js
@@ -3,16 +3,43 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+app.use(express.static('./public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(morgan('short'));
 
-app.get('/user/:id', (req, res) => {
-    console.log('Fetching user with Id:', req.params.id);
-    const connection = mysql.createConnection({
+function getConnection() {
+    return  mysql.createConnection({
         host: 'localhost',
         user: 'root',
         database: 'userDB'
     });
+}
+
+app.post('/user_create', (req, res) => {
+    console.log('First Name:', req.body.create_first_name);
+    const firstName = req.body.create_first_name;
+    const lastName = req.body.create_last_name;
+
+    const queryString = "INSERT into Users (firstName, lastName) values (?, ?)";
+    getConnection().query(queryString, [firstName, lastName], (err, results, fields) => {
+        if(err) {
+            console.log('Failed to add User');
+            res.sendStatus(500);
+            res.end();
+        } else {
+            console.log('Inserted a new record with id:', results.id);
+        }
+    })
+    res.end();
+});
+
+app.get('/user/:id', (req, res) => {
+    console.log('Fetching user with Id:', req.params.id);
+    const connection = getConnection()
     const userId = req.params.id;
     const queryString = "SELECT * FROM USERS WHERE id = ?";
     connection.query(queryString, [userId], (err, rows, fields) => {
