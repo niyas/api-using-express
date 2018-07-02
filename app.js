3afe13d9060848655f1b6e37557b0b3e6ba7c514
Mohammed Niyas <Mohammedniyas@MSSPLLAP066.meritgroup.com>
2018-07-02 17:42:52 +0100

    added get endppoint for users

--- app.js
+++ app.js
@@ -2,8 +2,29 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const mysql = require('mysql');
 
 app.use(morgan('short'));
+
+app.get('/user/:id', (req, res) => {
+    console.log('Fetching user with Id:', req.params.id);
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'userDB'
+    });
+    const userId = req.params.id;
+    const queryString = "SELECT * FROM USERS WHERE id = ?";
+    connection.query(queryString, [userId], (err, rows, fields) => {
+        if(err) {
+            console.log('Failed to query for user', err);
+            res.sendStatus(500);
+            res.end();
+        } else {
+            res.json(rows);
+        }
+    })
+});
 
 app.get("/", (req, res) => {
     console.log('Responding to root route');
